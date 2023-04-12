b3f38642f5e89412dc59c51f739553a11488406f
jakub.grabowski <kubagrabowski1903@gmail.com>
2023-04-12 11:45:08 +0200

    Add shadow to TopAppBars, change MainScreen topbar to be left-aligned. Add icon for no results in search.

--- app/src/main/java/com/example/marvelcomics/ui/screens/components/ComicTopAppBar.kt
+++ app/src/main/java/com/example/marvelcomics/ui/screens/components/ComicTopAppBar.kt
@@ -7,14 +7,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ComicTopAppBar(
+    modifier: Modifier = Modifier,
+    isForMainScreen: Boolean = false,
     title: String = "Screen",
+    titleFontSize: TextUnit = 20.sp,
+    titleFontWeight: FontWeight = FontWeight.Normal,
     icon: ImageVector? = null,
     actionIcon: ImageVector? = null,
     colors: TopAppBarColors? = null,
@@ -29,41 +37,84 @@ fun ComicTopAppBar(
 
     val topAppBarColors = colors ?: defaultTopAppBarColors
 
-    CenterAlignedTopAppBar(
-        title = {
-            Text(
-                text = title,
-                fontSize = 20.sp,
-                color = MaterialTheme.colors.onBackground
-            )
-        },
-        navigationIcon = {
-            if (icon != null) {
-                IconButton(onClick = { onNavigationIconClicked.invoke() }) {
-                    Icon(
-                        imageVector = icon,
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.onBackground
-                    )
+    if (isForMainScreen) {
+        TopAppBar(
+            modifier = modifier,
+            title = {
+                Text(
+                    text = title,
+                    fontSize = titleFontSize,
+                    fontWeight = titleFontWeight,
+                    color = MaterialTheme.colors.onBackground
+                )
+            },
+            navigationIcon = {
+                if (icon != null) {
+                    IconButton(onClick = { onNavigationIconClicked.invoke() }) {
+                        Icon(
+                            imageVector = icon,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onBackground
+                        )
+                    }
+                } else {
+                    Box {}
                 }
-            } else {
-                Box {}
-            }
-        },
-        actions = {
-            if (actionIcon != null) {
-                IconButton(onClick = { onActionIconClicked.invoke() }) {
-                    Icon(
-                        imageVector = actionIcon,
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.onBackground
-                    )
+            },
+            actions = {
+                if (actionIcon != null) {
+                    IconButton(onClick = { onActionIconClicked.invoke() }) {
+                        Icon(
+                            imageVector = actionIcon,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onBackground
+                        )
+                    }
+                } else {
+                    Box {}
                 }
-            } else {
-                Box {}
-            }
-        },
-        colors = topAppBarColors,
-        scrollBehavior = scrollBehavior
-    )
+            },
+            colors = topAppBarColors
+        )
+    } else {
+        CenterAlignedTopAppBar(
+            modifier = modifier,
+            title = {
+                Text(
+                    text = title,
+                    fontSize = titleFontSize,
+                    fontWeight = titleFontWeight,
+                    color = MaterialTheme.colors.onBackground
+                )
+            },
+            navigationIcon = {
+                if (icon != null) {
+                    IconButton(onClick = { onNavigationIconClicked.invoke() }) {
+                        Icon(
+                            imageVector = icon,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onBackground
+                        )
+                    }
+                } else {
+                    Box {}
+                }
+            },
+            actions = {
+                if (actionIcon != null) {
+                    IconButton(onClick = { onActionIconClicked.invoke() }) {
+                        Icon(
+                            imageVector = actionIcon,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onBackground
+                        )
+                    }
+                } else {
+                    Box {}
+                }
+            },
+            colors = topAppBarColors,
+            scrollBehavior = scrollBehavior
+        )
+    }
 }
