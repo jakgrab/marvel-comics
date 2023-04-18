3b48d0f8b616c494d8136ee3aa634e571dba04f0
jakub.grabowski <kubagrabowski1903@gmail.com>
2023-04-18 09:07:05 +0200

    Tests don't work for now

--- app/src/main/java/com/example/marvelcomics/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/marvelcomics/ui/screens/main/MainViewModel.kt
@@ -12,6 +12,7 @@ import com.example.marvelcomics.data.wrapper.DataOrException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -48,8 +49,12 @@ class MainViewModel @Inject constructor(private val comicRepository: ComicReposi
                 offset = currentPage * Constants.PAGE_SIZE
             )
 
+            val maxComics: Int = _comicsData.value.data.let {
+                it?.data?.total ?: 0
+            }
+
             isEndReached = if (_comicsData.value.data != null) {
-                currentPage * Constants.PAGE_SIZE >= _comicsData.value.data!!.data.total
+                currentPage * Constants.PAGE_SIZE >= maxComics
             } else {
                 true
             }
@@ -63,10 +68,12 @@ class MainViewModel @Inject constructor(private val comicRepository: ComicReposi
     }
 
     fun getComicByTitle(title: String) {
-        viewModelScope.launch(Dispatchers.Default) {
+        viewModelScope.launch {
             _comicsDataByTitle.value = comicRepository.getComicsByTitle(title)
         }
     }
 
-    fun cancelSearch() { _comicsDataByTitle.value.data = null }
+    fun cancelSearch() {
+        _comicsDataByTitle.value.data = null
+    }
 }
--- app/src/test/java/com/example/marvelcomics/ui/screens/main/MainViewModelTest.kt
+++ app/src/test/java/com/example/marvelcomics/ui/screens/main/MainViewModelTest.kt
@@ -1,5 +1,99 @@
 package com.example.marvelcomics.ui.screens.main
 
-import org.junit.Assert.*
+import com.example.marvelcomics.data.model.Comics
+import com.example.marvelcomics.data.model.Data
+import com.example.marvelcomics.data.model.Result
+import com.example.marvelcomics.data.repository.ComicRepository
+import com.example.marvelcomics.data.wrapper.DataOrException
+import com.example.marvelcomics.util.MainCoroutineRule
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class MainViewModelTest
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(JUnit4::class)
+class MainViewModelTest {
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+//    @ExperimentalCoroutinesApi
+//    private val dispatcher = StandardTestDispatcher()
+//
+//    @get:Rule
+//    val rule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: MainViewModel
+
+
+    private var fakeResult =
+        mockk<Result>(relaxed = true)
+
+    private var fakeData = mockk<Data>(relaxed = true)
+
+    private var fakeComics = mockk<Comics>(relaxed = true)
+
+    private val fakeDataOrException =
+        mockk<DataOrException<Comics, Boolean, Exception>>(relaxed = true)
+
+    private val fakeRepository: ComicRepository = mockk {
+        coEvery { getComicsByTitle(any()) } returns fakeDataOrException
+    }
+
+    @Before
+    fun setup() {
+//        Dispatchers.setMain(dispatcher)
+
+        fakeResult = fakeResult.copy(title = "Spider-man", description = "description")
+        fakeData = fakeData.copy(results = arrayListOf(fakeResult, fakeResult, fakeResult))
+        fakeComics = fakeComics.copy(data = fakeData)
+
+        fakeDataOrException.data = fakeComics
+
+        fakeRepository.apply {
+            coEvery {
+                getComics(0)
+            } returns DataOrException(data = fakeComics)
+
+            coEvery {
+                getComicsByTitle(any())
+            } returns fakeDataOrException
+        }
+        viewModel = MainViewModel(comicRepository = fakeRepository)
+    }
+
+//    @ExperimentalCoroutinesApi
+//    @After
+//    fun clean() {
+//        Dispatchers.resetMain()
+//    }
+
+    @Test
+    fun `WHEN title starts with Spider RETURN Spider-man comics`() = runTest {
+        viewModel.getComicByTitle(title = "Spider")
+        advanceUntilIdle()
+        val dataorexception = viewModel.comicsDataByTitle.value as DataOrException<Comics, Boolean, Exception>
+        val comics: Comics? = dataorexception.data
+        val data = comics?.data
+        val results = data?.results
+        val title = results?.get(0)?.title
+        assertEquals(
+            fakeResult.title,
+            title
+        )
+    }
+}
+
+class FakeDataOrException<Comics, Boolean, E: Exception>(
+    var data: Comics? = null,
+    var loading: Boolean? = null,
+    var exception: E? = null
+)
