4b16dbf6637929ff8865ba4a8ed06dc5b124f98f
jakub.grabowski <kubagrabowski1903@gmail.com>
2023-04-25 09:39:03 +0200

    Add roboto as app's font

--- feature-main/src/main/java/com/example/feature_main/ui/theme/Type.kt
+++ feature-main/src/main/java/com/example/feature_main/ui/theme/Type.kt
@@ -3,14 +3,25 @@ package com.example.feature_main.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.feature_main.R
 
+val Roboto = FontFamily(
+    fonts = listOf(
+        Font(R.font.roboto_medium, FontWeight.Medium),
+        Font(R.font.roboto_bold, FontWeight.Bold),
+        Font(R.font.roboto_light, FontWeight.Light),
+        Font(R.font.roboto_regular, FontWeight.Normal),
+        Font(R.font.roboto_thin, FontWeight.Thin),
+    )
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
@@ -32,7 +43,7 @@ val Typography.HeaderComicList: TextStyle
     @Composable
     get() {
         return  TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Roboto,
             fontWeight = FontWeight.W700,
             fontSize = 36.sp,
             lineHeight = 18.sp
@@ -43,7 +54,7 @@ val Typography.ComicTitle: TextStyle
     @Composable
     get() {
         return  TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Roboto,
             fontWeight = FontWeight.W500,
             fontSize = 20.sp,
             lineHeight = 21.sp
@@ -54,7 +65,7 @@ val Typography.ComicAuthorList: TextStyle
     @Composable
     get() {
         return  TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Roboto,
             fontWeight = FontWeight.W400,
             fontSize = 16.sp,
             lineHeight = 18.sp
@@ -65,7 +76,7 @@ val Typography.ComicDescriptionList: TextStyle
     @Composable
     get() {
         return  TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Roboto,
             fontWeight = FontWeight.W400,
             fontSize = 16.sp,
             lineHeight = 18.sp
